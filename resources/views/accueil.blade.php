1bbbbd982d3c5c51295167eab68228ff4e8f92a2
Youssef Taoufikallah <taoufikallahy@gmail.com>
2019-01-10 10:27:15 +0000

    initial commit

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,17 @@
+
+
+@extends('layouts.master')
+
+@section('content')
+
+
+<h1>la vue accueil</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Beatae fugit maiores autem voluptatibus libero ab. 
+    Vitae reiciendis minima enim quam nulla repudiandae.
+     Pariatur odit voluptas itaque, nisi atque veniam deserunt.
+</p>
+
+@endsection
---
+++ resources/views/service.blade.php
@@ -0,0 +1,11 @@
+
+
+@extends('layouts.master')
+
+@section('content')
+    <h1>la vue service</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A porro, 
+        perferendis quo ullam ad quae eos voluptatum nulla, totam harum quasi numquam molestiae, 
+        distinctio repellat deserunt inventore quia hic provident!</p>
+
+@endsection
